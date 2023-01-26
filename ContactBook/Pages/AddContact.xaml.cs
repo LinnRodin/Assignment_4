f973bf16fdba64416fb02eb3b6942b7ac56dd921
Linn Rodin <ael.rodin@gmail.com>
2023-01-26 21:58:43 +0100

    update

--- ContactBook/Pages/AddContact.xaml.cs
+++ ContactBook/Pages/AddContact.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using ContactBook.Models;
+using ContactBook.Services;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +20,25 @@ using System.Windows.Shapes;
 
 namespace ContactBook.Pages
 {
-    /// <summary>
-    /// Interaction logic for AddContact.xaml
-    /// </summary>
+    
     public partial class AddContact : Page
     {
+        private readonly FileManagerService fileManagerService;
+
         public AddContact()
         {
             InitializeComponent();
+            fileManagerService = new FileManagerService();
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            //fileManagerService.AddToList(new Contact() {tb_FirstName });
+            //tb_FirstName = string.Empty;
+
         }
     }
 }
---
+++ ContactBook/Services/FileManagerService.cs
@@ -0,0 +1,64 @@
+﻿using ContactBook.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.Services
+{
+    public class FileManagerService
+    {
+        private string filePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\content.json";
+        private List<Contact> contacts;
+
+        public FileManagerService()
+        {
+            ReadFromFile();
+        }
+
+        public void ReadFromFile()
+        {
+            try
+            {
+                using var sr = new StreamReader(filePath);
+                contacts = JsonConvert.DeserializeObject<List<Contact>>(sr.ReadToEnd())!;
+            }
+            catch { contacts = new List<Contact>(); }
+        }
+
+        public void SaveToFile()
+        {
+
+
+            using var sw = new StreamWriter(filePath);
+            sw.WriteLine(JsonConvert.SerializeObject(contacts));
+
+        }
+
+        public void AddToList(Contact content)
+        {
+            contacts.Add(content);
+            SaveToFile();
+        }
+
+        public void RemoveFromList(Contact content) 
+        {
+            contacts.Remove(content);
+            SaveToFile();
+        }
+
+
+        public ObservableCollection<Contact> Content()
+        { 
+            var items = new ObservableCollection<Contact>();
+            foreach (Contact contact in contacts)
+                items.Add(contact);
+            return items;
+        
+        }
+    }
+}
